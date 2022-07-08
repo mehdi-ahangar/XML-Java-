b149bb622732ee219ba23b73678ffa571dfab460
mehdi-ahangar <88955876+mehdi-ahangar@users.noreply.github.com>
2022-07-08 18:31:12 +0430

    Add files via upload

--- XmlDocumentInversion.java
+++ XmlDocumentInversion.java
@@ -1,55 +1,80 @@
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.ArrayList;
-import java.util.List;
-
+import java.util.Iterator;
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.events.Attribute;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
+
+import org.junit.Test;
 
 public class XmlDocumentInversion {
 
     public static void main(String[] args) {
 
-        List<String> Tags = new ArrayList<>();
+        ArrayList<ArrayList<String>> tags = new ArrayList<ArrayList<String>>();
         try {
 
             XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
-            XMLEventReader reader = xmlInputFactory.createXMLEventReader(new FileInputStream("program.xml"));
+            XMLEventReader reader = xmlInputFactory.createXMLEventReader(new FileInputStream("input.xml"));
 
             while (reader.hasNext()) {
                 XMLEvent nextEvent = reader.nextEvent();
                 if (nextEvent.isStartElement()) {
                     StartElement startElement = nextEvent.asStartElement();
-                    Tags.add(startElement.getName().toString());
+                    tags.add(new ArrayList<>());
+                    tags.get(tags.size() - 1).add(startElement.getName().toString());
 
-                }
-                if (nextEvent.isEndElement()) {
-
-                    String PrintLine = "", Margin = "";
-                    for (int i = Tags.size() - 1; i >= 0; i--) {
-                        Margin = Margin + "   ";
-                        if (i != 0)
-                            PrintLine = Margin + "  <" + Tags.get(i).toString() + ">";
-                        else
-                            PrintLine = Margin + "  <" + Tags.get(i).toString() + "/>";
-
-                        System.out.println(PrintLine);
+                    Iterator<Attribute> iterator = startElement.getAttributes();
+                    while (iterator.hasNext()) {
+                        Attribute attribute = iterator.next();
+                        tags.get(tags.size() - 1).add(attribute.toString());
                     }
-                    for (int i = 1; i < Tags.size(); i++) {
-                        Margin = Margin.substring(0, Margin.length() - 4);
-                        PrintLine = Margin + "  </" + Tags.get(i).toString() + ">";
-                        System.out.println(PrintLine);
-                    }
-
-                    return;
 
                 }
             }
+            String printLine = "", margin = "";
+            for (int i = tags.size() - 1; i >= 0; i--) {
+                margin = margin + "    ";
+                printLine = margin + "<";
+                for (int j = 0; j < tags.get(i).size(); j++)
+                    if (j != 0)
+                        printLine = printLine + "  " + tags.get(i).get(j).toString();
+                    else
+                        printLine = printLine + tags.get(i).get(j).toString();
+
+                if (i != 0)
+                    printLine = printLine + ">";
+                else
+                    printLine = printLine + "/>";
+
+                System.out.println(printLine);
+            }
+            for (int i = 1; i < tags.size(); i++) {
+                margin = margin.substring(0, margin.length() - 4);
+                printLine = margin + "  </" + tags.get(i).get(0).toString() + ">";
+                System.out.println(printLine);
+            }
+
+            return;
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            System.out.println("Error in parsing the XML file :(");
         }
 
+    }
+    @Test
+    public void testXmlDocumentInversionMain() throws IOException {
+        
+        String[] args = null;
+        System.setOut(new PrintStream(new FileOutputStream("output")));
+        System.setErr(new PrintStream(new FileOutputStream("error")));
+        System.setIn(new FileInputStream("input.xml"));
+        XmlDocumentInversion.main(args);
+        
     }
 
 }
